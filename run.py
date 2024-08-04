eaae44f47660b2ac3153b2f58a89635d75c83507
Matheus Rodrigues Vicente <matheus.rvicente@live.com>
2024-08-04 10:38:06 +0000

    creating variables

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+# Initialization of variables
+player_score = 5
+computer_score = 5
+turn = 5
+player_positions =[]
+computer_positions = []
+

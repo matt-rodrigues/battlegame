b179ab57f0cd1ba2dbf912668d5bf8d6aaff34e3
Matheus Rodrigues Vicente <matheus.rvicente@live.com>
2024-08-04 10:44:21 +0000

    creating game and player board

--- run.py
+++ run.py
@@ -10,3 +10,20 @@ turn = 5
 player_positions =[]
 computer_positions = []
 
+def create_board(rows, colus):
+    """
+    Creating a game board with the given numbers of rows and columns
+    """
+    board = []
+    for i in range(rows):
+        board.append(cols * [0])
+    return board
+
+def show_player_board():
+    """
+    Display the player's board
+    """
+    print("\nPlayer's Board")
+    for row in range(5):
+        print(player_board[row])
+    

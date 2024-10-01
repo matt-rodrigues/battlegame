46a92e4e84ab1c00dc2b755465d7eacb0df9af5a
Matheus Rodrigues Vicente <matheus.rvicente@live.com>
2024-10-01 19:38:16 +0000

    improving the code with validation for the username, board visualization for clarity of the game goes, pep8 adjustments

--- run.py
+++ run.py
@@ -4,158 +4,174 @@ from colorama import Fore, Style, init
 init()
 
 # Initialization of variables
-player_score = 5
-computer_score = 5
-turn = 0  # The turn should start from 0
-player_positions = []
-computer_positions = []
+PLAYER_SCORE = 5
+COMPUTER_SCORE = 5
+TURN = 0
+PLAYER_POSITIONS = []
+COMPUTER_POSITIONS = []
+PLAYER_GUESSES = []  # To track player's guesses
+COMPUTER_GUESSES = []  # To track computer's guesses
 
+BOARD_SIZE = 5
+ROWS = 'ABCDE'
+
+# PEP8: Constants use uppercase, and line lengths are limited to 79 characters
 
 def create_board(size):
-    """
-    Create a game board with the given number of rows and columns.
-    """
-    board = []
-    for _ in range(size):
-        board.append([0] * size)
-    return board
+    """Create an empty game board."""
+    return [['O'] * size for _ in range(size)]  # 'O' for unguessed positions
 
 
-def show_player_board():
+def display_board(board, is_player=True):
     """
-    Display the player's board.
+    Display the board with row and column labels.
+    is_player: If True, shows player's board; otherwise shows the computer's.
     """
-    print(f"\n{player_name}'s Board")
-    for row in player_board:
-        print(row)
+    print("  1  2  3  4  5")
+    for i, row in enumerate(board):
+        row_label = ROWS[i]
+        print(f"{row_label} {'  '.join(row)}")
 
 
-def show_computer_board():
+def is_valid_position(pos, guesses, size=BOARD_SIZE):
     """
-    Display the computer's board with hidden positions.
-    """
-    print("\nComputer's Board")
-    for row in hidden_computer_board:
-        print(row)
-    print('------------------------------------')
-
-
-def is_valid_position(pos, size=5):
-    """
-    Check if the given position is valid within the board size.
+    Check if a given position is valid.
+    - Ensures it's within the board's range.
+    - Ensures it's not a repeated guess.
     """
     if len(pos) != 2:
         return False
-    if pos[0] not in 'ABCDE' or not pos[1].isdigit():
+    if pos[0] not in ROWS or not pos[1].isdigit():
         return False
     if int(pos[1]) < 1 or int(pos[1]) > size:
         return False
+    if pos in guesses:
+        return False  # Already guessed this position
     return True
 
 
-def setup_boards():
+def setup_board(board, player=False):
     """
-    Set up boards for the player and computer by placing ships.
+    Place 5 ships randomly on the board for the player or the computer.
     """
-    global player_board, computer_board, hidden_computer_board
-
-    player_board = create_board(board_size)
-    computer_board = create_board(board_size)
-    hidden_computer_board = create_board(board_size)
-
+    positions = []
     for i in range(5):
-        player_valid = False
-        computer_valid = False
-
-        while not player_valid:
-            xy = input(f'Enter the position for your {i+1}º ship (A1 to E{board_size}): ').upper()
-            if is_valid_position(xy, board_size) and xy not in player_positions:
-                x = 'ABCDE'.index(xy[0])
-                y = int(xy[1]) - 1
-                player_board[x][y] = 1
-                player_positions.append(xy)
-                player_valid = True
+        valid = False
+        while not valid:
+            if player:
+                pos = input(f'Enter the position for your {i+1}º ship (A1 to E5): ').upper()
             else:
-                print("Invalid or occupied position, try again.")
-
-        while not computer_valid:
-            x = randint(0, 4)
-            y = randint(0, 4)
-            xy = f'{chr(x + ord("A"))}{y + 1}'
-            if xy not in computer_positions:
-                computer_board[x][y] = 1
-                computer_positions.append(xy)
-                computer_valid = True
-
-
-def main_game_loop():
-    """
-    Main game loop for the battleship game
-    """
-    global turn, player_score, computer_score
-
-    while True:
-        if player_score == 0 or computer_score == 0:
-            break
-        else:
-            if turn % 2 == 0:  # Player attacks
-                attack_pos = input(
-                    Fore.YELLOW + f'Enter the position you want to attack (A1 to E{board_size}), {player_name}: ' + Style.RESET_ALL
-                ).upper()
-                if is_valid_position(attack_pos, board_size):
-                    x = 'ABCDE'.index(attack_pos[0])
-                    y = int(attack_pos[1]) - 1
-                    if computer_board[x][y] == 1:
-                        hidden_computer_board[x][y] = 'X'
-                        computer_score -= 1
-                        print(Fore.GREEN + 'You hit a ship!' + Style.RESET_ALL)
-                    else:
-                        print(Fore.RED + 'You missed!' + Style.RESET_ALL)
-                    turn += 1
-                else:
-                    print(Fore.RED + 'Invalid position, try again.' + Style.RESET_ALL)
-            else:  # Computer attacks
                 x = randint(0, 4)
-                y = randint(0, 4)  # Correct range for the board
-                attack_pos = f'{chr(x + ord("A"))}{y + 1}'
-                if player_board[x][y] == 1:
-                    player_board[x][y] = 'X'
-                    player_score -= 1
-                    print(Fore.RED + f"The computer hit your ship at {attack_pos}!" + Style.RESET_ALL)
+                y = randint(0, 4)
+                pos = f'{ROWS[x]}{y+1}'
+
+            if is_valid_position(pos, positions):
+                x = ROWS.index(pos[0])
+                y = int(pos[1]) - 1
+                board[x][y] = 'S'  # 'S' for ship
+                positions.append(pos)
+                valid = True
+            elif player:
+                print(Fore.RED + "Invalid or occupied position, try again." + Style.RESET_ALL)
+    return positions
+
+
+def update_guess_board(board, guess, hit):
+    """Update the guess board with 'X' for hit and 'M' for miss."""
+    x = ROWS.index(guess[0])
+    y = int(guess[1]) - 1
+    board[x][y] = 'X' if hit else 'M'  # 'M' for miss
+
+
+def main_game_loop(player_board, computer_board, hidden_computer_board):
+    """
+    Main game loop for the battleship game.
+    - Prevents repeated guesses.
+    - Differentiates between hit/miss/unguessed.
+    """
+    global TURN, PLAYER_SCORE, COMPUTER_SCORE
+    while PLAYER_SCORE > 0 and COMPUTER_SCORE > 0:
+        if TURN % 2 == 0:  # Player's turn
+            attack_pos = input(
+                Fore.YELLOW + f'{player_name}, enter position to attack (A1 to E5): ' + Style.RESET_ALL
+            ).upper()
+
+            if is_valid_position(attack_pos, PLAYER_GUESSES):
+                PLAYER_GUESSES.append(attack_pos)
+                x = ROWS.index(attack_pos[0])
+                y = int(attack_pos[1]) - 1
+
+                if computer_board[x][y] == 'S':  # Ship hit
+                    update_guess_board(hidden_computer_board, attack_pos, hit=True)
+                    COMPUTER_SCORE -= 1
+                    print(Fore.GREEN + "You hit a ship!" + Style.RESET_ALL)
                 else:
-                    print(f"The computer missed at {attack_pos}!")
-                turn += 1
+                    update_guess_board(hidden_computer_board, attack_pos, hit=False)
+                    print(Fore.RED + "You missed!" + Style.RESET_ALL)
+                TURN += 1
+            else:
+                print(Fore.RED + "Invalid or repeated position, try again." + Style.RESET_ALL)
+        else:  # Computer's turn
+            while True:
+                x = randint(0, 4)
+                y = randint(0, 4)
+                attack_pos = f'{ROWS[x]}{y+1}'
 
-            show_computer_board()
-            show_player_board()
-            print(Fore.MAGENTA + f"{player_name}'s Remaining Ships: {player_score}" + Style.RESET_ALL)
-            print(Fore.BLUE + f"Computer's Remaining Ships: {computer_score}" + Style.RESET_ALL)
+                if attack_pos not in COMPUTER_GUESSES:
+                    COMPUTER_GUESSES.append(attack_pos)
 
-    if player_score == 0:
-        print(f"{player_name}, you lost. The computer destroyed all your ships.")
+                    if player_board[x][y] == 'S':  # Ship hit
+                        player_board[x][y] = 'X'
+                        PLAYER_SCORE -= 1
+                        print(Fore.RED + f"The computer hit your ship at {attack_pos}!" + Style.RESET_ALL)
+                    else:
+                        player_board[x][y] = 'M'
+                        print(f"The computer missed at {attack_pos}.")
+                    TURN += 1
+                    break
+
+        display_board(hidden_computer_board, is_player=False)
+        display_board(player_board)
+
+        print(Fore.MAGENTA + f"{player_name}'s Remaining Ships: {PLAYER_SCORE}" + Style.RESET_ALL)
+        print(Fore.BLUE + f"Computer's Remaining Ships: {COMPUTER_SCORE}" + Style.RESET_ALL)
+
+    if PLAYER_SCORE == 0:
+        print(f"{player_name}, you lost! The computer destroyed all your ships.")
     else:
         print(f"Congratulations, {player_name}! You destroyed all the computer's ships.")
 
 
-# Introduction
+# Get the player's name with validation
+def get_player_name():
+    """Ask for the player's name and ensure it is not left blank."""
+    while True:
+        name = input("Please enter your name: ").strip()
+        if name:
+            return name
+        else:
+            print(Fore.RED + "Name cannot be blank, please enter a valid name." + Style.RESET_ALL)
+
+
+# Introduction and game setup
 print("Welcome to the Battleship Game!")
 print("You will place 5 ships on a 5x5 board.")
-print("Each ship occupies one cell on the board.")
-print("You will take turns with the computer to attack each other's ships.")
-print("The goal is to destroy all of the opponent's ships.")
+print("Try to destroy all the computer's ships before it destroys yours.")
 
-# Get the player's name
-player_name = input("Please enter your name: ")
+player_name = get_player_name()
 
-# Board size
-board_size = 5
+# Create boards
+player_board = create_board(BOARD_SIZE)
+computer_board = create_board(BOARD_SIZE)
+hidden_computer_board = create_board(BOARD_SIZE)
 
-# Setup boards with ships
-setup_boards()
+# Setup boards
+PLAYER_POSITIONS = setup_board(player_board, player=True)
+COMPUTER_POSITIONS = setup_board(computer_board)
 
 # Show initial boards
-show_computer_board()
-show_player_board()
+display_board(hidden_computer_board, is_player=False)
+display_board(player_board)
 
 # Start the main game loop
-main_game_loop()
+main_game_loop(player_board, computer_board, hidden_computer_board)
